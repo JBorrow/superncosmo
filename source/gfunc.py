d65e325438f213f66c823bb4e4bd3548ecf9bb5f
Josh Borrow <josh.borrow@gmail.com>
2015-10-20 11:52:23 +0100

    added functions for computing initial omega_lambda

--- source/gfunc.py
+++ source/gfunc.py
@@ -11,6 +11,7 @@
 ##########################################################################"""
 
 import numpy as np
+import scipy.integrate as si
 
 
 ################## Set global variables here ################################
@@ -19,7 +20,7 @@ import numpy as np
 H0 = 75. * (1/3.086E19)  # in km/s/mpc convert to s^-1
 c = 299792458.  # in m/s
 pi = np.pi
-m0_init=0
+m0_init=-15
 low_z_data = "../data/low_z_data.csv"
 
 
@@ -28,7 +29,7 @@ low_z_data = "../data/low_z_data.csv"
 
 def get_mag_from_flux(flux, m0=m0_init):
     """m0 in units of...""" # FIX ME
-    return -20 - 2.5*np.log10(flux)
+    return m0 - 2.5*np.log10(flux)
 
 
 def GR_factor_R0(z, c=c, H0=H0):
@@ -54,3 +55,14 @@ def get_data(fname=low_z_data):
     as the delimiter"""
     raw = np.loadtxt(fname, delimiter=" ", usecols=(1,2,3))
     return raw[:,0], raw[:,1], raw[:,2]
+
+
+def get_h_flat(z, omega_lambda, H0=H0):
+    """Gets H in a flat universe as a function of z only"""
+    return H0*np.sqrt((1-omega_lambda)*(1+z)**3 + omega_lambda)
+
+
+def GR_factor_integral(z, omega_lambda, c=c, H0=H0, get_h_flat=get_h_flat):
+    """Gets the factor R0 eta based on the model in get_h_flat by integration"""
+    initial_result = c*si.quad(get_h_flat, a=0, b=z, args=(omega_lambda, H0))
+    return initial_result[0]
